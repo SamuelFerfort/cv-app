3dcafac7667c73dded7d08d86e799ab992a6cce6
Samuel <samuferfort@gmail.com>
2024-05-30 04:13:44 +0200

    fix edit btn

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,55 +1,60 @@
-export default function Info({ formData, handleSubmit, handleChange, isSubmitted }) {
-    return (
-      <section>
-        <h1>Personal Information</h1>
-        <form action="/" name="infoForm" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="nameInput">Name</label>
-            {isSubmitted.infoForm ? (
-              formData.nameInput
-            ) : (
-              <input
-                type="text"
-                name="nameInput"
-                required
-                value={formData.nameInput}
-                onChange={handleChange}
-              />
-            )}
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-  
-            {isSubmitted.infoForm ? (
-              formData.email
-            ) : (
-              <input
-                type="email"
-                name="email"
-                required
-                value={formData.email}
-                onChange={handleChange}
-              />
-            )}
-          </div>
-          <div>
-            <label htmlFor="tel">Tel</label>
-            {isSubmitted.infoForm ? (
-              formData.tel
-            ) : (
-              <input
-                type="tel"
-                required
-                name="tel"
-                value={formData.tel}
-                onChange={handleChange}
-              />
-            )}
-          </div>
-          <button type="submit">{isSubmitted ? "Edit" : "Submit"}</button>
-        </form>
-      </section>
-    );
-  }
-  
-  
+export default function Info({
+  formData,
+  handleSubmit,
+  handleChange,
+  isSubmitted,
+}) {
+  return (
+    <section>
+      <h1>Personal Information</h1>
+      <form action="/" name="infoForm" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nameInput">Name</label>
+          {isSubmitted.infoForm ? (
+            formData.nameInput
+          ) : (
+            <input
+              type="text"
+              name="nameInput"
+              required
+              value={formData.nameInput}
+              onChange={handleChange}
+            />
+          )}
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+
+          {isSubmitted.infoForm ? (
+            formData.email
+          ) : (
+            <input
+              type="email"
+              name="email"
+              required
+              value={formData.email}
+              onChange={handleChange}
+            />
+          )}
+        </div>
+        <div>
+          <label htmlFor="tel">Tel</label>
+          {isSubmitted.infoForm ? (
+            formData.tel
+          ) : (
+            <input
+              type="tel"
+              required
+              name="tel"
+              value={formData.tel}
+              onChange={handleChange}
+            />
+          )}
+        </div>
+        <button type="submit">
+          {isSubmitted.infoForm ? "Edit" : "Submit"}
+        </button>
+      </form>
+    </section>
+  );
+}
